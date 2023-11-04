b1502914b0f6f87d7787999052f1378068f51476
xukuan <93767276+XKKuan@users.noreply.github.com>
2023-11-04 19:56:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parser.add_argument("--device",type =str,default="cuda:0",nargs="?",help="cpu,cu
 parser.add_argument("--size_m", type=int, default=200000, nargs="?",help="the number of mission.")
 parser.add_argument("--batch_size", type=int, default=1024, nargs="?",help="batch size")
 parser.add_argument("--lr", type=float, default=0.001, nargs="?",help="learning rate ")
-parser.add_argument("--meta_rate", type=float, default=5.0, nargs="?",help="meta rate ")
+parser.add_argument("--meta_rate", type=float, default=4.0, nargs="?",help="meta rate ")
 parser.add_argument("--DrugMeta",type=bool,default=True,nargs='?',help="use DrugMeta or not.")
 parser.add_argument("--DrugLearner",type=str,default='GCN',nargs='?',help="Which drug learner to use MLP/GCN.")
 args = parser.parse_args()
